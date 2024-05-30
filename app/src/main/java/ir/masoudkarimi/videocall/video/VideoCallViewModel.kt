37d41c5cd2be48b5aa02d43b8f9d8b8afc4cf3b7
masoud <karimi.masoud5494@gmail.com>
2024-05-30 19:12:57 +0330

    create and join call

--- app/src/main/java/ir/masoudkarimi/videocall/video/VideoCallViewModel.kt
+++ app/src/main/java/ir/masoudkarimi/videocall/video/VideoCallViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import io.getstream.video.android.core.StreamVideo
+import kotlinx.coroutines.launch
 
 class VideoCallViewModel(
     private val videoClient: StreamVideo
@@ -21,7 +23,7 @@ class VideoCallViewModel(
     fun onAction(action: VideoCallAction) {
         when (action) {
             VideoCallAction.JoinCall -> {
-
+                joinCall()
             }
 
             VideoCallAction.OnDisconnect -> {
@@ -30,5 +32,34 @@ class VideoCallViewModel(
         }
     }
 
+    private fun joinCall() {
+        if (state.callState == CallState.ACTIVE) {
+            return
+        }
 
+        viewModelScope.launch {
+            state = state.copy(callState = CallState.JOINING)
+
+            val shouldCreate = videoClient
+                .queryCalls(filters = emptyMap())
+                .getOrNull()
+                ?.calls
+                ?.isEmpty() == true
+
+
+            state.call.join(create = shouldCreate)
+                .onSuccess {
+                    state = state.copy(
+                        callState = CallState.ACTIVE,
+                        error = null
+                    )
+                }
+                .onError {
+                    state = state.copy(
+                        error = it.message,
+                        callState = null
+                    )
+                }
+        }
+    }
 }
